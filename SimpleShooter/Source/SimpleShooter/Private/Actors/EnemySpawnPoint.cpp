4758db64d42b4f424f0e1839532d00c1e1220db8
gforget <gforget2@gmail.com>
2024-10-17 16:04:41 -0400

    created spawn icon

--- SimpleShooter/Source/SimpleShooter/Private/Actors/EnemySpawnPoint.cpp
+++ SimpleShooter/Source/SimpleShooter/Private/Actors/EnemySpawnPoint.cpp
@@ -3,13 +3,29 @@
 
 #include "Actors/EnemySpawnPoint.h"
 
+#include "Components/BillboardComponent.h"
+
 
 // Sets default values
 AEnemySpawnPoint::AEnemySpawnPoint()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	// Create a billboard component
+	UBillboardComponent* Billboard = CreateDefaultSubobject<UBillboardComponent>(TEXT("Billboard"));
+	Billboard->SetupAttachment(RootComponent);
+
+	// Load a custom texture to use as the icon (must be in the project content)
+	static ConstructorHelpers::FObjectFinder<UTexture2D> IconTexture(TEXT("/Game/Texture/SpawnNPCIcon"));
+    
+	if (IconTexture.Succeeded())
+	{
+		Billboard->SetSprite(IconTexture.Object);
+	}
 }
+
+
 
 // Called when the game starts or when spawned
 void AEnemySpawnPoint::BeginPlay()
