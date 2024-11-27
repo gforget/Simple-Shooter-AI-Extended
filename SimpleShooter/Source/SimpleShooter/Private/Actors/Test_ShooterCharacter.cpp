941055e9c7c9202fa6a733524a31af0599f4fdd6
gforget <gforget2@gmail.com>
2024-11-26 22:36:30 -0500

    WIP - Test going into level map

---
+++ SimpleShooter/Source/SimpleShooter/Private/Actors/Test_ShooterCharacter.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Test_ShooterCharacter.h"
+
+
+// Sets default values
+ATest_ShooterCharacter::ATest_ShooterCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATest_ShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATest_ShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATest_ShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATest_ShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &ATest_ShooterCharacter::LookUpRate);
+	
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ATest_ShooterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &ATest_ShooterCharacter::LookRightRate);
+}
+
+void ATest_ShooterCharacter::MoveForward(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector()*AxisValue);
+}
+
+void ATest_ShooterCharacter::MoveRight(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector()*AxisValue);
+}
+
+void ATest_ShooterCharacter::LookUpRate(float AxisValue)
+{
+	AddControllerPitchInput(AxisValue * RotationRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ATest_ShooterCharacter::LookRightRate(float AxisValue)
+{
+	AddControllerYawInput(AxisValue * RotationRate * GetWorld()->GetDeltaSeconds());
+}
+
---
+++ SimpleShooter/Source/SimpleShooter/Public/Actors/Test_ShooterCharacter.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Test_ShooterCharacter.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API ATest_ShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATest_ShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 100.0f;
+	
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
